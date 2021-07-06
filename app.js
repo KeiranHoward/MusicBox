d4ae85b33fd2126c524a5fe77821d53569f2ce0e
Keiran <keiranhoward.dev@gmail.com>
2021-07-06 16:27:24 +1000

    added javascript event listeners to play sounds

---
+++ app.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+    const sound = document.querySelectorAll('.sound');
+    const pads = document.querySelectorAll('.pads div');
+
+    pads.forEach((pad, index) => {
+        pad.addEventListener("click", function() {
+                sound[index].currentTime = 0;
+            sound[index].play();
+        });
+    });
+});
